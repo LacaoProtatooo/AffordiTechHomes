3695c32da1b880d5082e8b84220780724ff2b6c5
JoshNotheGreat <Joshziemenntan998@gmail.com>
2024-03-07 12:41:25 +0800

    Update create.blade.php

--- resources/views/schedule/create.blade.php
+++ resources/views/schedule/create.blade.php
@@ -19,7 +19,7 @@
         <label for="property_id" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"></label><b>Property:</b></label><br><br>
         <select name="property_id" id="property_id" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
             <option value="" disabled selected>Select Property Number</option>
-            @foreach ($propertyid as $property)
+            @foreach ($properties as $property)
                 <option value="{{$property->id}}">Property ID {{$property->id}}</option>
             @endforeach
         </select><br>
