66cff9504ec28cb1e7d455bac1d2f8edaf0c9cfa
JoshNotheGreat <Joshziemenntan998@gmail.com>
2024-03-10 18:01:26 +0800

    Create 2024_03_10_093737_create_visits_table.php

---
+++ database/migrations/2024_03_10_093737_create_visits_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('visits', function (Blueprint $table) {
+            $table->id()->autoIncrement();
+            $table->string('approval_status');
+            $table->dateTime('schedule');
+            $table->unsignedBigInteger('customer_id');
+            $table->unsignedBigInteger('agent_id');
+            $table->unsignedBigInteger('property_id');
+            $table->string('approval_status');
+
+            // FK
+            $table->foreign('customer_id')->references('id')->on('customers')->onDelete('cascade');
+            $table->foreign('agent_id')->references('id')->on('agents')->onDelete('cascade');
+            $table->foreign('property_id')->references('id')->on('properties')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('visits');
+    }
+};
