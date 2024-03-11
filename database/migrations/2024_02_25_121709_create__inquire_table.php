da71841a5b9dd59816ad56cb9efc0156ffba4713
JoshNotheGreat <Joshziemenntan998@gmail.com>
2024-03-11 19:51:10 +0800

    Create 2024_02_25_121709_create__inquire_table.php

---
+++ database/migrations/2024_02_25_121709_create__inquire_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('inquiries', function (Blueprint $table) {
+            $table->unsignedBigInteger('property_id');
+            $table->unsignedBigInteger('customer_id');
+            $table->unsignedBigInteger('agent_id')->nullable();
+            $table->unsignedBigInteger('broker_id');
+
+            // FK
+            $table->foreign('customer_id')->references('id')->on('customers')->onDelete('cascade');
+            $table->foreign('property_id')->references('id')->on('properties')->onDelete('cascade');
+            $table->foreign('broker_id')->references('id')->on('brokers')->onDelete('cascade');
+            $table->foreign('agent_id')->references('id')->on('agents')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('inquiries');
+    }
+};
