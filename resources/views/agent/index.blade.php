07a10552ba81bd8dff2f1075efa4ca1ab770be9e
JoshNotheGreat <Joshziemenntan998@gmail.com>
2024-03-12 20:52:08 +0800

    Update index.blade.php

--- resources/views/agent/index.blade.php
+++ resources/views/agent/index.blade.php
@@ -22,14 +22,6 @@
 
         <div class="items-center justify-between hidden w-full md:flex md:w-auto md:order-1 bg-transparent" id="navbar-cta">
             <div class="flex flex-col font-medium p-4 md:p-0 mt-0 border border-gray-100 rounded-lg bg-transparent md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-transparent dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
-                {{-- Add Property --}}
-                <button onclick="location.href='{{ route('property.create') }}';" class="relative inline-flex items-center justify-center p-0.5 mb-2 me-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-teal-300 to-lime-300 group-hover:from-teal-300 group-hover:to-lime-300 dark:text-white dark:hover:text-gray-900 focus:ring-4 focus:outline-none focus:ring-lime-200 dark:focus:ring-lime-800">
-                    <span class="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">Add Property</span>
-                </button>
-                {{-- Add Schedules --}}
-                <button onclick="location.href='{{ route('schedule.create') }}';" class="relative inline-flex items-center justify-center p-0.5 mb-2 me-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-teal-300 to-lime-300 group-hover:from-teal-300 group-hover:to-lime-300 dark:text-white dark:hover:text-gray-900 focus:ring-4 focus:outline-none focus:ring-lime-200 dark:focus:ring-lime-800">
-                    <span class="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">Add Schedule</span>
-                </button>
                 {{-- Check Appointments --}}
                 <button onclick="location.href='{{ route('agent.appointment') }}';" class="relative inline-flex items-center justify-center p-0.5 mb-2 me-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-teal-300 to-lime-300 group-hover:from-teal-300 group-hover:to-lime-300 dark:text-white dark:hover:text-gray-900 focus:ring-4 focus:outline-none focus:ring-lime-200 dark:focus:ring-lime-800">
                     <span class="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">Check Appointments</span>
@@ -39,7 +31,7 @@
                     <span class="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">Check Inquiries</span>
                 </button>
                 {{-- Transaction --}}
-                <button onclick="location.href='{{route('agent.transaction')}}';" class="relative inline-flex items-center justify-center p-0.5 mb-2 me-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-teal-300 to-lime-300 group-hover:from-teal-300 group-hover:to-lime-300 dark:text-white dark:hover:text-gray-900 focus:ring-4 focus:outline-none focus:ring-lime-200 dark:focus:ring-lime-800">
+                <button onclick="location.href='';" class="relative inline-flex items-center justify-center p-0.5 mb-2 me-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-teal-300 to-lime-300 group-hover:from-teal-300 group-hover:to-lime-300 dark:text-white dark:hover:text-gray-900 focus:ring-4 focus:outline-none focus:ring-lime-200 dark:focus:ring-lime-800">
                     <span class="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">Sold Properties</span>
                 </button>
             </div>
@@ -96,66 +88,6 @@
                     </tr>
                 </thead>
                 <tbody>
-                    @foreach ($properties as $property)
-                    @php
-                        $imagePaths = explode(',', $property->image_path);
-                    @endphp
-                    
-                    <tr class="bg-gradient-to-r from-teal-200 to-lime-200 hover:bg-gradient-to-l hover:from-teal-200 hover:to-lime-200 focus:ring-4 focus:outline-none focus:ring-lime-200 border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-green-700 dark:hover:bg-gray-600">
-                        
-                        <td class="px-6 py-4 text-right">
-                            @if ($property->status_of_approval != 'pending')
-                                <a href="{{route('property.edit',$property->id)}}" class="font-medium text-blue-600 dark:text-blue-500 hover:underline">Edit</a>
-                            @else
-                                <p href="" class="font-medium text-blue-600 dark:text-blue-500 hover:underline" disabled>Uneditable due to Pending</p>
-                            @endif
-                        </td>
-                        <td class="px-6 py-4 text-right">
-                            <a href="#" onclick="confirmDelete('{{ route('property.delete', $property->id) }}')" class="font-medium text-blue-600 dark:text-blue-500 hover:underline">Delete</a>
-                        </td>
-                        <td class="px-6 py-4 text-right">
-                            @if ($property->status_of_approval)
-                                {{ $property->status_of_approval }}
-                            @else
-                                Pending
-                            @endif
-                        </td>
-                        <td class="px-6 py-4">
-                            {{ $property->property_type }}
-                        </td>
-                        <td class="px-6 py-4">
-                            {{ $property->address }}
-                        </td>
-                        <td class="px-6 py-4">
-                            {{ $property->price }}
-                        </td>
-                        <td class="px-6 py-4">
-                            {{ $property->description }}
-                        </td>
-                        <td class="px-6 py-4">
-                            {{ $property->rooms }}
-                        </td>
-                        <td class="px-6 py-4">
-                            {{ $property->sqm }}
-                        </td>
-                        <td class="px-6 py-4">
-                            {{ $property->cr }}
-                        </td>
-                        <td class="px-6 py-4">
-                            {{ $property->status }}
-                        </td>
-                        <td class="px-6 py-4">
-                            {{ $property->parking }}
-                        </td>
-
-                        <td class="px-6 py-4 flex space-x-2">
-                            @foreach($imagePaths as $imagePath)
-                                <img src="{{ asset($imagePath) }}" alt="Property Image" style="max-width: 100px; max-height: 150px;">
-                            @endforeach      
-                        </td> 
-                    </tr>
-
-                    @endforeach
                 </tbody>
             </table>
         </div>
