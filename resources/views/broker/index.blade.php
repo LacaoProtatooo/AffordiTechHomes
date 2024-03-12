9d6b64a2dad11d78e152593a1dbe051281508d35
JoshNotheGreat <Joshziemenntan998@gmail.com>
2024-03-12 20:36:08 +0800

    Update index.blade.php

--- resources/views/broker/index.blade.php
+++ resources/views/broker/index.blade.php
@@ -50,68 +50,97 @@
 
 
 
-    {{-- TABLE --}}
-    <div class = "items-center justify-between mb-10 ml-10 mr-10 mt-10 w-full md:w-auto md:order-1 bg-green-200">
-        <div class="relative overflow-x-auto shadow-2xl sm:rounded-lg">
-            <table class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400 bg-green-900">
-                <thead class="text-xs text-white uppercase dark:bg-gray-700 dark:text-gray-400 bg-green-900">
-                    <tr>
-                        <th scope="col" class="px-6 py-3">
-                            <span class="sr-only">Edit</span>
-                        </th>
-                        <th scope="col" class="px-6 py-3">
-                            <span class="sr-only">Delete</span>
-                        </th>
-                        <th scope="col" class="px-6 py-3">
-                            Approval Status
-                        </th>
-                        <th scope="col" class="px-6 py-3">
-                            Property Type
-                        </th>
-                        <th scope="col" class="px-6 py-3">
-                            Address
-                        </th>
-                        <th scope="col" class="px-6 py-3">
-                            Price
-                        </th>
-                        <th scope="col" class="px-6 py-3">
-                            Description
-                        </th>
-                        <th scope="col" class="px-6 py-3">
-                            Rooms
-                        </th>
-                        <th scope="col" class="px-6 py-3">
-                            Sq Meters
-                        </th>
-                        <th scope="col" class="px-6 py-3">
-                            CR
-                        </th>
-                        <th scope="col" class="px-6 py-3">
-                            Availability
-                        </th>
-                        <th scope="col" class="px-6 py-3">
-                            Parking
-                        </th>
-                        <th scope="col" class="px-6 py-3">
-                            Images
-                        </th>
-                    </tr>
-                </thead>
-                <tbody>
-                </tbody>
+  {{-- TABLE --}}
+  <div class = "items-center justify-between mb-10 ml-10 mr-10 mt-10 w-full md:w-auto md:order-1 bg-green-200">
+    <div class="relative overflow-x-auto shadow-2xl sm:rounded-lg">
+        <table class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400 bg-green-900">
+            <thead class="text-xs text-white uppercase dark:bg-gray-700 dark:text-gray-400 bg-green-900">
+                <tr>
+                    <th scope="col" class="px-6 py-3">
+                        Developer
+                    </th>
+                    <th scope="col" class="px-6 py-3">
+                        Property Type
+                    </th>
+                    <th scope="col" class="px-6 py-3">
+                        Address
+                    </th>
+                    <th scope="col" class="px-6 py-3">
+                        Price
+                    </th>
+                    <th scope="col" class="px-6 py-3">
+                        Description
+                    </th>
+                    <th scope="col" class="px-6 py-3">
+                        Rooms
+                    </th>
+                    <th scope="col" class="px-6 py-3">
+                        Sq Meters
+                    </th>
+                    <th scope="col" class="px-6 py-3">
+                        CR
+                    </th>
+                    <th scope="col" class="px-6 py-3">
+                        Status
+                    </th>
+                    <th scope="col" class="px-6 py-3">
+                        Parking
+                    </th>
+                    <th scope="col" class="px-6 py-3">
+                        Images
+                    </th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach ($properties as $property)
+                @php
+                    $imagePaths = explode(',', $property->image_path);
+                @endphp
                 
-            </table>
-        </div>
+                <tr class="bg-gradient-to-r from-teal-200 to-lime-200 hover:bg-gradient-to-l hover:from-teal-200 hover:to-lime-200 focus:ring-4 focus:outline-none focus:ring-lime-200 border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-green-700 dark:hover:bg-gray-600">
+                    <td class="px-6 py-4">
+                        {{ $property->developer }}
+                    </td>
+                    <td class="px-6 py-4">
+                        {{ $property->property_type }}
+                    </td>
+                    <td class="px-6 py-4">
+                        {{ $property->address }}
+                    </td>
+                    <td class="px-6 py-4">
+                        {{ $property->price }}
+                    </td>
+                    <td class="px-6 py-4">
+                        {{ $property->description }}
+                    </td>
+                    <td class="px-6 py-4">
+                        {{ $property->rooms }}
+                    </td>
+                    <td class="px-6 py-4">
+                        {{ $property->sqm }}
+                    </td>
+                    <td class="px-6 py-4">
+                        {{ $property->cr }}
+                    </td>
+                    <td class="px-6 py-4">
+                        {{ $property->status }}
+                    </td>
+                    <td class="px-6 py-4">
+                        {{ $property->block }}
+                    </td>
+
+                    <td class="px-6 py-4 flex space-x-2">
+                        @foreach($imagePaths as $imagePath)
+                            <img src="{{ asset($imagePath) }}" alt="Property Image" style="max-width: 100px; max-height: 150px;">
+                        @endforeach      
+                    </td> 
+                </tr>
+
+                @endforeach
+            </tbody>
+        </table>
     </div>
-
-    <script>
-        function confirmDelete(deleteUrl) {
-            if (confirm("Are you sure you want to delete this property?")) {
-                window.location.href = deleteUrl;
-            }
-        }
-    </script>
-
+</div>
     @include('common.footer')
 </body>
 </html>
