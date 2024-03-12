ca6485d11585745808cff2459f5462020876d374
JoshNotheGreat <Joshziemenntan998@gmail.com>
2024-03-12 23:23:32 +0800

    Create Propertybroker.php

---
+++ app/Models/Propertybroker.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Propertybroker extends Model
+{
+    use HasFactory;
+    protected $table = 'property_has_broker';
+    protected $fillable = [
+        'property_id',
+        'customer_id', 
+    ];
+}
