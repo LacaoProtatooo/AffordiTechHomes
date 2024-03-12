1687da6b63cb0e79d5e38974449067ef9dd7f4c9
JoshNotheGreat <Joshziemenntan998@gmail.com>
2024-03-13 00:32:27 +0800

    Update PropertyBroker.php

--- app/Models/PropertyBroker.php
+++ app/Models/PropertyBroker.php
@@ -5,13 +5,13 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class PropertyBroker extends Model
+class Propertybroker extends Model
 {
     use HasFactory;
     protected $table = 'property_has_broker';
     protected $fillable = [
         'property_id',
-        'broker_id',
+        'customer_id', 
         'created_at',
         'updated_at',
     ];
