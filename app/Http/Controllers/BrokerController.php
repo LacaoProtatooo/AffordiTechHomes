1f6e5f5cbcb42d44e7db0af4ffd12107ab7973e1
JoshNotheGreat <Joshziemenntan998@gmail.com>
2024-03-13 12:11:25 +0800

    Update BrokerController.php

--- app/Http/Controllers/BrokerController.php
+++ app/Http/Controllers/BrokerController.php
@@ -74,12 +74,11 @@ class BrokerController extends Controller
     }
 
     public function inquireassign($customer_id, $property_id, $agent_id) {
-        // Update the agent_id for the specified customer_id and property_id
+        
         Inquire::where('customer_id', $customer_id)
                ->where('property_id', $property_id)
                ->update(['agent_id' => $agent_id]);
     
-        // Redirect to the appropriate route
         return redirect()->route('broker.dashboard');
     }
     
